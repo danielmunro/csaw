bd871e299d37098bbc365a936e0d236a0df09fa8
Dan Munro <dan@danmunro.com>
2019-10-07 21:37:55 -0500

    extract to method

--- src/action/event_consumer/input_to_action_event_consumer.c
+++ src/action/event_consumer/input_to_action_event_consumer.c
@@ -1,19 +1,23 @@
+void dispatch_action_event(GameServiceT *game_service, Event *event, ActionT *action) {
+    Event *action_event = create_action_event(action);
+    dispatch_event(game_service, action_event);
+    if (action_event->status == LiveEventStatus) {
+        Request *request = create_request(
+                action->action_type,
+                event->mob,
+                event->buffer);
+        action->action(game_service, request);
+        free(request);
+    }
+    free(action_event);
+}
+
 void consume_input_to_action_event(GameServiceT *game_service, Event *event) {
     for (int i = 0; i < MAX_ACTIONS; i++) {
         ActionT *action = get_action(game_service, event->buffer);
         if (action) {
-            Event *action_event = create_action_event(game_service->action_table->actions[i]);
-            dispatch_event(game_service, action_event);
-            if (action_event->status == LiveEventStatus) {
-                Request *request = create_request(
-                        action->action_type,
-                        event->mob,
-                        event->buffer);
-                action->action(game_service, request);
-                free(request);
-            }
-            free(action_event);
-            return;
+           dispatch_action_event(game_service, event, action);
+           return;
         }
     }
 }
