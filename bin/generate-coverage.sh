261a25a6277cb4437424403cf045287a58a26ece
Dan Munro <dan@danmunro.com>
2019-10-18 21:37:02 -0500

    coverage

---
+++ bin/generate-coverage.sh
@@ -0,0 +1,6 @@
+set -e
+
+rm -f coverage.info coverage2.info test_main.gcno a.out
+gcc -I/usr/local/opt/libpq/include -lpq -ftest-coverage -fprofile-arcs tests/test_main.c
+lcov --capture --directory . --output-file coverage.info
+lcov --list coverage.info
