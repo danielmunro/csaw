c26153a3ff9d54e42ac148f1284d343e7fc0e100
Dan Munro <dan@danmunro.com>
2019-10-22 19:37:39 -0500

    cmake insanity

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/danmunro/projects/csaw
-# Build directory: /Users/danmunro/projects/csaw
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_main "/Users/danmunro/projects/csaw/build/test_main")
-set_tests_properties(test_main PROPERTIES  _BACKTRACE_TRIPLES "/Users/danmunro/projects/csaw/CMakeLists.txt;14;add_test;/Users/danmunro/projects/csaw/CMakeLists.txt;0;")
